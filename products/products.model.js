95f02904fca27816d7e7448e5ba31e8284cbe897
Kevin Mas <kmas@thess-corp.fr>
2023-02-03 08:32:56 +0100

    feat: adding mutations to graphql server

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
 		id: "redshoe",
 		description: "Red Shoe",
 		price: 42.12,
+		reviews: [],
 	},
 	{
 		id: "bluejean",
 		description: "Blue Jeans",
 		price: 55.55,
+		reviews: [],
 	},
 ];
 
@@ -27,8 +29,35 @@ function getProductsById(id) {
 	});
 }
 
+function addNewProduct(id, description, price) {
+	const newProduct = {
+		id,
+		price,
+		description,
+		reviews: [],
+	};
+
+	products.push(newProduct);
+	return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+	const matchedProduct = getProductsById(id);
+
+	if (matchedProduct) {
+		const newProductReview = {
+			rating,
+			comment,
+		};
+		matchedProduct.reviews.push(newProductReview);
+		return newProductReview;
+	}
+}
+
 module.exports = {
 	getAllProducts,
 	getProductsByPrice,
 	getProductsById,
+	addNewProduct,
+	addNewProductReview,
 };
